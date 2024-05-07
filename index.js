41f9d5df96bd90d2f78a97d15bc9d3b44b4d16b4
tuii999 <altuntas17nam1311@outlook.com>
2024-05-07 17:20:50 +0700

    commit

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-const isPalindrome = (str) => str === str.split("").reverse().join("");
+function uniquePaths(m, n) {
+  const dp = new Array(m).fill(0).map(() => new Array(n).fill(0));
+  for (let i = 0; i < m; i++) {
+    dp[i][0] = 1;
+  }
+  for (let j = 0; j < n; j++) {
+    dp[0][j] = 1;
+  }
+  for (let i = 1; i < m; i++) {
+    for (let j = 1; j < n; j++) {
+      dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+    }
+  }
+  return dp[m - 1][n - 1];
+}
